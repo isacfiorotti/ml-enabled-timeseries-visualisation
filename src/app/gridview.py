76a59a2706cf8e574d9547fc2c9913ef8a945c21
isacfiorotti <115185340+isacfiorotti@users.noreply.github.com>
2024-06-21 16:49:43 +0100

    Migrate GridView class to gridview.py

--- src/app/gridview.py
+++ src/app/gridview.py
@@ -1,1 +1,24 @@
 import tkinter as tk
+
+class GridView(tk.Frame):
+    def __init__(self, parent, grid_size, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.grid_size = grid_size
+        self.create_grid()
+
+    def create_grid(self):
+        self.cols = int(sqrt(self.grid_size))
+        self.rows = ceil(self.grid_size / self.cols)
+
+        if self.rows * self.cols < self.grid_size:
+            self.cols += 1
+        
+        cell_count = 0
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if cell_count < self.grid_size:
+                    button = tk.Button(self, relief='flat')
+                    button.grid(row=i, column=j, sticky="nsew")
+                    self.grid_columnconfigure(j, weight=1)
+                    cell_count += 1
+            self.grid_rowconfigure(i, weight=1)
--- src/app/gui.py
+++ src/app/gui.py
@@ -41,33 +41,7 @@ class MainApp(tk.Frame):
         self.bottom.pack(side='bottom', fill='both', expand=True)
         self.background.add(self.bottom, stretch='always')
         
-
         #lineview
         self.line_view = LineView(self.bottom)
         self.line_view.pack(fill='both', expand=True)
-        
-# Possibly migrate this to a different script and import the functions as needed and change to canvas instead of buttons for better results
-
-class GridView(tk.Frame):
-    def __init__(self, parent, grid_size, *args, **kwargs):
-        super().__init__(parent, *args, **kwargs)
-        self.grid_size = grid_size
-        self.create_grid()
-
-    def create_grid(self):
-        self.cols = int(sqrt(self.grid_size))
-        self.rows = ceil(self.grid_size / self.cols)
-
-        if self.rows * self.cols < self.grid_size:
-            self.cols += 1
-        
-        cell_count = 0
-        for i in range(self.rows):
-            for j in range(self.cols):
-                if cell_count < self.grid_size:
-                    button = tk.Button(self, relief='flat')
-                    button.grid(row=i, column=j, sticky="nsew")
-                    self.grid_columnconfigure(j, weight=1)
-                    cell_count += 1
-            self.grid_rowconfigure(i, weight=1)
 
