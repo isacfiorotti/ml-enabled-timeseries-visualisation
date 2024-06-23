d1a1364e7d7751cf48d6f6e83cc56547329b941d
isacfiorotti <115185340+isacfiorotti@users.noreply.github.com>
2024-06-23 14:53:20 +0100

    Add toggle to treemap

--- src/app/vis_mediator.py
+++ src/app/vis_mediator.py
@@ -4,9 +4,17 @@ class VisMediator():
         self.treemap = treemap
         self.grid_view = grid_view
         self.line_view = line_view
+        self.toggled_nodes = {} # By creating a toggled node dict we can improve efficiency instead of checking for all nodes every time
 
-    def on_treemap_click(self):
-        self.grid_view.set_cell_color('cell1', 'red')
+    def on_treemap_click(self, node, toggle):
+        if toggle == False:
+            self.treemap.nodes[node]['toggle'] = True
+            self.toggled_nodes[node] = self.treemap.nodes[node]
+        else:
+            if self.treemap.nodes[node] == self.toggled_nodes[node]:
+                del self.toggled_nodes[node]
+            self.treemap.nodes[node]['toggle'] = False
+        
 
     def on_treemap_enter(self, node, color):
         self.grid_view.create_grid_view()
@@ -18,4 +26,11 @@ class VisMediator():
 
     def on_treemap_leave(self):
         self.grid_view.create_grid_view()
-
+        
+    def resolve_treemap_toggles(self):
+        for node in self.toggled_nodes:
+            signals_in_node = self.relation_manager.get_signals_in_node(node)
+            for signal in signals_in_node:
+                cell = self.relation_manager.get_signal_cell(signal)
+                color = self.toggled_nodes[node]['color']
+                self.grid_view.set_cell_color(cell, color)
