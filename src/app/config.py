777acaca496b7c223d61e819669884ed495ee4fd
isacfiorotti <115185340+isacfiorotti@users.noreply.github.com>
2024-06-29 22:38:44 +0100

    Add file_path to config.py

--- src/app/config.py
+++ src/app/config.py
@@ -1,2 +1,3 @@
 WINDOW_TITLE = "My Application"
 WINDOW_SIZE = "400x400"
+FILE_PATH = '/Users/isacfiorotti/Documents/COMP4031 - Personal Project/tkinter-health-data-visualisation/src/app/data/data.csv'
