d8301221af261059b1364bfba2cbbaf39aad6f64
isacfiorotti <115185340+isacfiorotti@users.noreply.github.com>
2024-06-30 10:39:25 +0100

    Add rounded tabs

--- src/app/views/tabs.py
+++ src/app/views/tabs.py
@@ -4,13 +4,37 @@ class Tabs(tk.Frame):
     def __init__(self, parent, data_mediator, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         self.data_mediator = data_mediator
-        self.canvas = tk.Canvas(self, height=20) # height needs to be consistent with initiation in gui.py
+        self.canvas = tk.Canvas(self, height=23) # height needs to be consistent with initiation in gui.py
         self.canvas.pack(fill='both', expand=True)
         self.create_data_tabs(self.data_mediator.get_headers())
 
-    def on_resize():
+    def on_resize(self):
         #TODO Change tab width based on window width
         pass
+
+    def create_rounded_rectangle(self, x1, y1, x2, y2, radius=2, **kwargs):
+        points = [x1 + radius, y1,
+                  x1 + radius, y1,
+                  x2 - radius, y1,
+                  x2 - radius, y1,
+                  x2, y1,
+                  x2, y1 + radius,
+                  x2, y1 + radius,
+                  x2, y2 - radius,
+                  x2, y2 - radius,
+                  x2, y2,
+                  x2 - radius, y2,
+                  x2 - radius, y2,
+                  x1 + radius, y2,
+                  x1 + radius, y2,
+                  x1, y2,
+                  x1, y2 - radius,
+                  x1, y2 - radius,
+                  x1, y1 + radius,
+                  x1, y1 + radius,
+                  x1, y1]
+
+        return self.canvas.create_polygon(points, **kwargs, smooth=True)
 
     def create_data_tabs(self, headers):
         rect_width = 120
@@ -26,11 +50,12 @@ class Tabs(tk.Frame):
             x2 = x_start + (i + 1) * rect_width + i * 3
             y2 = y_start + rect_height
             
-            self.canvas.create_rectangle(
+            self.create_rounded_rectangle(
                 x1,  # x1 position with 3 pixels space between rectangles
                 y1,  # y1 position
                 x2,  # x2 position
                 y2,  # y2 position
+                radius=10,  # Radius for rounded corners
                 fill='#DCDCDC',  # Fill color for rectangles
                 outline='#DCDCDC'  # Outline color for rectangles
             )
@@ -43,8 +68,8 @@ class Tabs(tk.Frame):
                 text_y,  
                 text=f'{headers[i]}',  # Text content
                 fill='grey',  # Text color
-                font=('Arial', 7)
+                font=('Arial', 10) 
             )
         
     def set_vis_mediator(self, vis_mediator):
-        self.vis_midiator = vis_mediator
+        self.vis_mediator = vis_mediator
