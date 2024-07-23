6e56f5d02c61ee95772c273b6d586b61877d9666
isacfiorotti <115185340+isacfiorotti@users.noreply.github.com>
2024-07-23 14:01:57 +0100

    Add UI overhaul

--- src/app/views/lineview.py
+++ src/app/views/lineview.py
@@ -38,22 +38,48 @@ class LineView(tk.Frame):
         self.data_y = y.values
 
         self.fig, self.ax = plt.subplots(figsize=(10, 6))
-        self.fig.subplots_adjust(bottom=0.15)  # Adjust the bottom margin
+        self.fig.patch.set_facecolor('#f0f0f0')
+        self.fig.subplots_adjust(bottom=0.2)
 
         self.line, = self.ax.plot(self.data_x[:self.display_count], self.data_y[:self.display_count], lw=0.5)
         self.ax.set_xlim(0, self.display_count - 1)
         self.ax.set_ylim(np.min(self.data_y[:self.display_count]), np.max(self.data_y[:self.display_count]))
         self.update_x_axis_labels(0)
 
+        self.ax.spines['top'].set_visible(False)
+        self.ax.spines['right'].set_visible(False)
+        self.ax.spines['bottom'].set_color('gray')
+        self.ax.spines['left'].set_color('gray')
+        self.ax.grid(color = 'white', linestyle = '--', linewidth = 0.5)
+        self.ax.set_facecolor('#ADC4CE')
+        self.ax.xaxis.label.set_color('grey')
+        self.ax.yaxis.label.set_color('grey')
+        self.ax.tick_params(axis='x', colors='grey')
+        self.ax.tick_params(axis='y', colors='grey')
+        self.line.set_color('#053B50')
+
         # Connect the events to the handlers
         self.canvas_fig.mpl_connect('button_press_event', self.on_press)
         self.canvas_fig.mpl_connect('motion_notify_event', self.on_motion)
         self.canvas_fig.mpl_connect('button_release_event', self.on_release)
 
-        self.ax_slider_index = plt.axes([0.1, 0.05, 0.8, 0.02], facecolor='lightgoldenrodyellow')
-        self.slider_index = Slider(self.ax_slider_index, '', 0, len(self.data_x) - self.display_count, valinit=0, valstep=1)
-        self.slider_index.valtext.set_visible(False)  # Hide the value text
+        # Slider for changing the start index
+        self.ax_slider_index = plt.axes([0.1, 0.1, 0.8, 0.02], facecolor='lightgoldenrodyellow')
+        self.slider_index = Slider(self.ax_slider_index, 'Idx.', 0, len(self.data_x) - self.display_count, valinit=0, valstep=1)
+        self.slider_index.label.set_fontsize(6)
+        self.slider_index.valtext.set_visible(False) 
         self.slider_index.on_changed(self.slider_index_update)
+        self.slider_index.label.set_color('grey')
+        self.slider_index.poly.set_facecolor('#ADC4CE')
+
+        # Slider for changing the display count
+        self.ax_slider_display_count = plt.axes([0.1, 0.05, 0.8, 0.02], facecolor='lightgoldenrodyellow')
+        self.slider_display_count = Slider(self.ax_slider_display_count, 'Freq.', 100, len(self.data_x), valinit=500, valstep=100)
+        self.slider_display_count.label.set_fontsize(6)
+        self.slider_display_count.valtext.set_visible(False)
+        self.slider_display_count.on_changed(self.slider_display_count_update)
+        self.slider_display_count.label.set_color('grey')
+        self.slider_display_count.poly.set_facecolor('#ADC4CE')
 
         return self.fig
 
@@ -63,7 +89,7 @@ class LineView(tk.Frame):
         self.canvas_fig.draw()
         self.canvas_fig.get_tk_widget().pack(fill=tk.BOTH, expand=True)
         self.fig = fig
-        self.fig.canvas.draw_idle()  # Force a redraw to adjust the layout
+        self.fig.canvas.draw_idle()
 
     def update_display(self, start_index):
         if start_index < 0 or start_index + self.display_count > len(self.data_x):
@@ -74,7 +100,6 @@ class LineView(tk.Frame):
         self.ax.set_xlim(start_index, start_index + self.display_count - 1)
         self.ax.set_ylim(np.min(new_data_y), np.max(new_data_y))
         self.update_x_axis_labels(start_index)
-        
         self.fig.canvas.draw_idle()
 
     def update_x_axis_labels(self, start_index):
@@ -111,3 +136,8 @@ class LineView(tk.Frame):
     def slider_index_update(self, val):
         start_index = int(val)
         self.update_display(start_index)
+
+    def slider_display_count_update(self, val):
+        self.display_count = int(val)
+        self.slider_index.valmax = len(self.data_x) - self.display_count
+        self.update_display(int(self.slider_index.val))
